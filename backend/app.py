22a4d4b6f08bdc8516ab08e683beb47994f820cd
NandVinchhi <nand.vinchhi@gmail.com>
2020-08-23 00:58:24 +0530

    init

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,13 @@
+import wikipedia
+
+def get_info(k):
+    a = wikipedia.search(k)[0]
+    b = wikipedia.summary(a)
+
+    c = wikipedia.page(a).url
+    d = wikipedia.page(a).images[0]
+    return {"text":b, "url":c, "image":d}
+
 from pymongo import MongoClient
 from pymongo.collection import ObjectId
 import bcrypt
@@ -5,10 +15,10 @@ from flask import Flask, request
 app = Flask(__name__)
 
 from flask_cors import CORS
-
+import math
 import json
 from bson import ObjectId
-
+import requests
 CORS(app)
 
 class JSONEncoder(json.JSONEncoder):
@@ -48,11 +58,27 @@ def login(email, password):
 def get_monument_data(name):
     return db.monuments.find_one({"name":name})
 
+def get_nearby(lat, lon, d, id):
+    a = abs(float(d["geometry"]["location"]["lat"]) - lat)
+    b = abs(float(d["geometry"]["location"]["lng"]) - lon)
+    k = round(math.sqrt(a * a + b * b), 1)
+
+    return {"id":id, "title":d["name"], "description":str(k) + " mi away", "image":d["icon"], "link":"https://www.google.com/search?q=" + d["name"], "maps":"https://www.google.com/maps/search/?api=1&query=" + str(lat) + "," + str(lon)}
+
+
+def get_all(lat, lon):
+    final = []
+    x = requests.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=monuments&&radius=10000&key=AIzaSyB7L122E6_uJGXT6uheNw95q2cmrp7UClQ&location=" + str(lat) + "," + str(lon)).json()
+    for i in range(1, 7):
+        k = x["results"][i - 1]
+        final.append(get_nearby(lat, lon, k, i))
+    return {"first":final[0], "second":final[1], "third":final[2], "fourth":final[3], "fifth":final[4], "sixth":final[5]}
+print(get_all(0, 0))
 @app.route('/monuments', methods=["GET", "POST"])
 def get_monument_info_endpoint():
     data = request.json
 
-    return JSONEncoder().encode(get_monument_data(data["name"]))
+    return get_info(data["name"])
 
 @app.route('/register', methods=["GET", "POST"])
 def register_endpoint():
@@ -66,5 +92,12 @@ def login_endpoint():
 
     return login(data["email"], data["password"])
 
+@app.route('/maps', methods=["GET", "POST"])
+def maps_endpoint():
+    data = request.json
+
+    return get_all(float(data["lat"]), float(data["lon"]))
+
 if __name__ == "__main__":
     app.run()
+
